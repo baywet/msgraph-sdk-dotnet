60462930c676387d802fc959828f5d41d95eed9f
Vincent Biret <vibiret@microsoft.com>
2024-08-02 16:11:32 -0400

    feat: clears old comments

---
+++ scripts/clearOldComments.ps1
@@ -0,0 +1,8 @@
+
+$prNumber = 1
+$comments = gh api --method GET -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/$($Env:GITHUB_REPOSITORY)/issues/$prNumber/comments" 
+            | ConvertFrom-Json
+            | ? {$_.user.login -eq "github-actions[bot]" -and $_.body.StartsWith("<!--publicapi-->")}
+foreach($comment in $comments) {
+    gh api --method DELETE -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/$($Env:GITHUB_REPOSITORY)/pulls/comments/$($comment.id)"
+}
