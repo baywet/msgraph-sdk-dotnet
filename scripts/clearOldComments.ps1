b9a8bbee28a26e36cb1b6992648930d86e2fe137
Vincent Biret <vibiret@microsoft.com>
2024-08-02 16:17:13 -0400

    feat: logging for comments deletion

--- scripts/clearOldComments.ps1
+++ scripts/clearOldComments.ps1
@@ -4,5 +4,6 @@ $comments = gh api --method GET -H "Accept: application/vnd.github+json" -H "X-G
             | ConvertFrom-Json
             | ? {$_.user.login -eq "github-actions[bot]" -and $_.body.StartsWith("<!--publicapi-->")}
 foreach($comment in $comments) {
+    Write-Host "Deleting comment $($comment.id)"
     gh api --method DELETE -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/$($Env:GITHUB_REPOSITORY)/pulls/comments/$($comment.id)"
 }
